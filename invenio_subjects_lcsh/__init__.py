360e94cfb57548f52e151ad19243dcd371f0c72c
Guillaume Viger <fenekku@fenekku.com>
2024-03-21 15:43:22 -0400

    release: v2023.11.2 [+]
    - Enable trusted publishing

--- invenio_subjects_lcsh/__init__.py
+++ invenio_subjects_lcsh/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021-2023 Northwestern University.
+# Copyright (C) 2021-2024 Northwestern University.
 #
 # invenio-subjects-lcsh is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -9,4 +9,4 @@
 """LCSH subject terms for InvenioRDM."""
 
 
-__version__ = '2023.11.1'
+__version__ = '2023.11.2'
